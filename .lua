0ae1a8895d9445def0a943ae38a429bb1795fa75
Aisen11394 <154973366+Aisen11394@users.noreply.github.com>
2024-04-08 16:58:07 +0900

    Update .lua

--- .lua
+++ .lua
@@ -1,4 +1,4 @@
 game:GetService("RunService").RenderStepped:wait()
 while task.wait() do
-  game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame * CFrame.fromEulerAnglesXYZ(1,0,0)
+  game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame * CFrame.fromEulerAnglesXYZ(1,0,1)
 end
